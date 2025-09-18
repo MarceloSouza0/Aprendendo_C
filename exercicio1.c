717b45ddea322f4529d857166bfeeb23f6dd165f
MarceloSouza0 <marceloprofissional2024@gmail.com>
2025-09-18 15:17:13 -0300

    .

--- exercicio1.c
+++ exercicio1.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int fibonacci(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
 int main() {
-	printf("Ola, mundo!");
+	int n = 15;
+	printf("Fibonacci (Recursivo): ");
+	for (int i = 0; i < n; i++) {
+		printf("%d", fibonacci(i));
+	}
+
+	return 0;
 }
