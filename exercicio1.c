72434081424798a349df502aee8c595bfee966dc
MarceloSouza0 <marceloprofissional2024@gmail.com>
2025-09-18 17:14:08 -0300

    tentando de novo

--- exercicio1.c
+++ exercicio1.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fibonacci(int n) {
+#include "main.h"
+
+
+int fib_recursivo(int n) {
 	if (n == 0) return 0;
 	if (n == 1) return 1;
-	return fibonacci(n - 1) + fibonacci(n - 2);
+	return fib_recursivo(n - 1) + fib_recursivo(n - 2);
 }
 
 
-int main() {
+int fibonacci_recursivo() { // O(2^n)
 	int n = 15;
 	printf("Fibonacci (Recursivo): ");
 	for (int i = 0; i < n; i++) {
-		printf("%d", fibonacci(i));
+		printf("%d", fib_recursivo(i));
 	}
 
 	return 0;
+}
+
+
+int fibonacci_iterativo() { // O(n)
+	int n = 15;
+    int a = 0, b = 1, c;
+
+    printf("Fibonacci (iterativo): ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return 0;
 }
---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+
+
+int main() {
+	fibonacci_iterativo();
+	return 0;
+}
